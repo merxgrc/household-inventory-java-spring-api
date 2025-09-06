2b508d689b94abd716b2190520a3148ba2b83d91
merxgrc <mgarcia73@mail.sfsu.edu>
2025-09-05 22:13:55 -0700

    chore(restructuring): adds the model, repo, service, and controller packages and moved ItemController into model package

--- src/main/java/com/mrxgrc/inventory/controller/ItemController.java
+++ src/main/java/com/mrxgrc/inventory/controller/ItemController.java
@@ -1,4 +1,4 @@
-package com.mrxgrc.inventory;
+package com.mrxgrc.inventory.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/test/java/com/mrxgrc/inventory/ItemControllerTest.java
+++ src/test/java/com/mrxgrc/inventory/ItemControllerTest.java
@@ -1,5 +1,6 @@
 package com.mrxgrc.inventory;
 
+import com.mrxgrc.inventory.controller.ItemController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
