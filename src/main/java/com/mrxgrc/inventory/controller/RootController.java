c39a53467e570ba16f7f0c4243bfda020a88bf1e
merxgrc <mgarcia73@mail.sfsu.edu>
2025-09-16 22:35:13 -0700

    Adds springdoc api dependencies to the pom.xml file, adds swagger-ui custom path to maven-wrapper.properties, adds redirectView to root endpoint in RootController.java

--- src/main/java/com/mrxgrc/inventory/controller/RootController.java
+++ src/main/java/com/mrxgrc/inventory/controller/RootController.java
@@ -2,6 +2,7 @@ package com.mrxgrc.inventory.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.Map;
 
@@ -9,17 +10,7 @@ import java.util.Map;
 public class RootController {
 
     @GetMapping("/")
-    public Map<String, Object> getRoot() {
-        return Map.of(
-                "message", "Hello World! Welcome to the Inventory API",
-                "endpoints", Map.of(
-                        "items", "/items",
-                        "getItemById", "/items/{id}",
-                        "createItem", "POST /items",
-                        "updateItem", "PUT /items/{id}",
-                        "deleteItem", "DELETE /items/{id}",
-                        "health", "/health"
-                )
-        );
+    public RedirectView redirectToSwagger() {
+        return new RedirectView("/swagger-ui/index.html");
     }
 }
